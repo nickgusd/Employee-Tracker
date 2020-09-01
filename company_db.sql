5733a6e3a7bab902a95b7c1c1e5a44e9b79b7ba3
Nick Guimarais <Nick.guimarais@gmail.com>
2020-08-31 17:03:59 -0700

    created company_db.sql file and created tables

---
+++ company_db.sql
@@ -0,0 +1,55 @@
+
+DROP DATABASE IF EXISTS company_db;
+
+CREATE DATABASE company_db;
+
+USE company_db;
+
+
+CREATE TABLE department (
+  id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(30) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+
+CREATE TABLE employee_role (
+id INT NOT NULL AUTO_INCREMENT,
+title VARCHAR(30) NOT NULL,
+salary DECIMAL(20, 2) NOT NULL, 
+department_id INT(10) NOT NULL,
+PRIMARY KEY (id)
+
+);
+
+CREATE TABLE employee (
+id INT NOT NULL AUTO_INCREMENT,
+first_name VARCHAR(30) NOT NULL,
+last_name VARCHAR(30) NOT NULL,
+role_id INT(10) NOT NULL,
+manager_id INT(10) NOT NULL,
+PRIMARY KEY (id)
+
+);
+
+
+
+-- * **department**:
+
+--   * **id** - INT PRIMARY KEY
+--   * **name** - VARCHAR(30) to hold department name
+
+-- * **role**:
+
+--   * **id** - INT PRIMARY KEY
+--   * **title** -  VARCHAR(30) to hold role title
+--   * **salary** -  DECIMAL to hold role salary
+--   * **department_id** -  INT to hold reference to department role belongs to
+
+-- * **employee**:
+
+--   * **id** - INT PRIMARY KEY
+--   * **first_name** - VARCHAR(30) to hold employee first name
+--   * **last_name** - VARCHAR(30) to hold employee last name
+--   * **role_id** - INT to hold reference to role employee has
+--   * **manager_id** - INT to hold reference to another employee that manager of the current employee. This field may be null if the employee has no manager
